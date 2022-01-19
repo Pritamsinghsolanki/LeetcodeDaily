d4f67259c175b7c4a6dcde4406f8d107eb2ff17e
shivam <65842693+Pritamsinghsolanki@users.noreply.github.com>
2022-01-19 17:59:14 +0530

    Add files via upload

---
+++ startingpointofloop.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode entry=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                while(slow!=entry){
+                    slow=slow.next;
+                    entry=entry.next;
+                }
+                return entry;
+            }
+        }
+        return null;
+    }
+}
