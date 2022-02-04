9c6569907378ac531b4ce9dc0534f26378ad2341
shivam <65842693+Pritamsinghsolanki@users.noreply.github.com>
2022-02-04 22:39:35 +0530

    Add files via upload

---
+++ Richestcustomer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        int n = accounts.length;
+        int m = accounts[0].length;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum += accounts[i][j];
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
